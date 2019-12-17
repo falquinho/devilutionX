c5e95fabdaa8cda4e763d4dee517b07c1647c475
falquinho <lucas.falcao.nb@gmail.com>
2019-12-17 11:32:40 -0300

    Improved the equipment comparison info.

--- SourceX/modern_interface/modern_info_box.cpp
+++ SourceX/modern_interface/modern_info_box.cpp
@@ -4,7 +4,9 @@
 
 DEVILUTION_BEGIN_NAMESPACE
 
-char compare_info[4][128] = {"", "", "", ""};
+#define MAX_COMPARE_LINES 7
+
+char compare_info[MAX_COMPARE_LINES][128] = {"", "", "", "", "", "", ""};
 
 char compare_loc = NUM_INVLOC;
 
@@ -29,7 +31,7 @@ void DrawMonsterInfo()
 void DrawCompareEquipInfo(Rect sister_rect)
 {
     Rect bkg = {0, 0, 0, 0};
-    for(int i = 0; i < 4; i++){
+    for(int i = 0; i < MAX_COMPARE_LINES; i++){
         if(compare_info[i][0] == '\0')
             break;
         int curr_w = strlen(&compare_info[i][0])*CHAR_W + 4;
@@ -44,7 +46,7 @@ void DrawCompareEquipInfo(Rect sister_rect)
 
     int offset = (bkg.w - (9*CHAR_W))/2;
     DrawString(bkg.x + offset, bkg.y, "EQUIPPED:");
-    for(int i = 0; i < 4; i++) {
+    for(int i = 0; i < MAX_COMPARE_LINES; i++) {
         if(compare_info[i][0] == '\0')
             break;
         offset = (bkg.w - (strlen(&compare_info[i][0]) * CHAR_W))/2;
@@ -94,7 +96,7 @@ void DrawModernInfoBox()
 
 void ClearComparisonInfo()
 {
-    for(int i = 0; i < 4; i++) {
+    for(int i = 0; i < MAX_COMPARE_LINES; i++) {
         compare_info[i][0] = '\0';
     }
     compare_loc = NUM_INVLOC;
@@ -143,19 +145,44 @@ void SetCompareEquipmentInfo(ItemStruct item)
     
     compare_loc = inv_loc;
 
-    strcpy(&compare_info[0][0], target_item._iName);
+    strcpy(&compare_info[0][0], target_item._iIName);
 
+    int curr_line = 1;
     if (target_item._iClass == ICLASS_WEAPON) {
 		if (target_item._iMaxDur == DUR_INDESTRUCTIBLE)
-			sprintf(&compare_info[1][0], "damage: %i-%i  Indestructible", target_item._iMinDam, target_item._iMaxDam);
+			sprintf(&compare_info[curr_line++][0], "damage: %i-%i  Indestructible", target_item._iMinDam, target_item._iMaxDam);
 		else
-			sprintf(&compare_info[1][0], "damage: %i-%i  Dur: %i/%i", target_item._iMinDam, target_item._iMaxDam, target_item._iDurability, target_item._iMaxDur);
+			sprintf(&compare_info[curr_line++][0], "damage: %i-%i  Dur: %i/%i", target_item._iMinDam, target_item._iMaxDam, target_item._iDurability, target_item._iMaxDur);
 	}
 	else if (target_item._iClass == ICLASS_ARMOR) {
 		if (target_item._iMaxDur == DUR_INDESTRUCTIBLE)
-			sprintf(&compare_info[1][0], "armor: %i  Indestructible", target_item._iAC);
+			sprintf(&compare_info[curr_line++][0], "armor: %i  Indestructible", target_item._iAC);
 		else
-			sprintf(&compare_info[1][0], "armor: %i  Dur: %i/%i", target_item._iAC, target_item._iDurability, target_item._iMaxDur);
+			sprintf(&compare_info[curr_line++][0], "armor: %i  Dur: %i/%i", target_item._iAC, target_item._iDurability, target_item._iMaxDur);
+	}
+
+    if(target_item._iPrePower != -1) {
+        PrintItemPower(target_item._iPrePower, &target_item);
+        sprintf(&compare_info[curr_line++][0], "%s", tempstr);
+    }
+
+    if(target_item._iSufPower != -1) {
+        PrintItemPower(target_item._iSufPower, &target_item);
+        sprintf(&compare_info[curr_line++][0], "%s", tempstr);
+    }
+
+    if (target_item._iMagical == ITEM_QUALITY_UNIQUE)
+		sprintf(&compare_info[curr_line++][0], "Unique item.");
+	
+    if (target_item._iMinMag + target_item._iMinDex + target_item._iMinStr) {
+		strcpy(tempstr, "Required:");
+		if (target_item._iMinStr)
+			sprintf(tempstr, "%s %i Str", tempstr, target_item._iMinStr);
+		if (target_item._iMinMag)
+			sprintf(tempstr, "%s %i Mag", tempstr, target_item._iMinMag);
+		if (target_item._iMinDex)
+			sprintf(tempstr, "%s %i Dex", tempstr, target_item._iMinDex);
+		sprintf(&compare_info[curr_line][0], tempstr);
 	}
 }
 
